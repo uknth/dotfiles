99c6f3bee253771dc2e53bd3cd7c6fc14d205ffa
Ujjwal Kanth <uknth@outlook.com>
2024-02-09 23:05:44 +0000

    fix: glyphs for cmp, offending outline keybinding

--- config/nvim_s/lua/plugins/outline.lua
+++ config/nvim_s/lua/plugins/outline.lua
@@ -5,7 +5,6 @@ return {
         cmd = { "Outline", "OutlineOpen" },
         keys = { -- Example mapping to toggle outline
             { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
-            { "<C-o>",     "<cmd>Outline<CR>", desc = "Toggle outline" },
         },
         opts = {},
     },
