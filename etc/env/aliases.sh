cf5eba476966a39330fdd431d00f246ef356e2cf
Ujjwal Kanth <uknth@outlook.com>
2025-10-02 08:01:07 +0000

    fix: add ghostty configuration, modify other properties, neovim update

--- etc/env/aliases.sh
+++ etc/env/aliases.sh
@@ -50,3 +50,5 @@ alias zola-build='docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app 
 alias zola-run-remote='docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app -p 8080:8080 ghcr.io/getzola/zola:v0.19.1 serve --interface 0.0.0.0 --port 8080'
 alias zola-run-wsl='docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app -p 8080:8080 ghcr.io/getzola/zola:v0.19.1 serve --interface 0.0.0.0 --port 8080 --base-url "http://127.0.0.1"'
 alias zola-run='docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app -p 8080:8080 ghcr.io/getzola/zola:v0.19.1 serve --interface 0.0.0.0 --port 8080 --base-url workstation-1.hnet'
+alias zola-run-remote='docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app -p 8080:8080 ghcr.io/getzola/zola:v0.19.1 serve --interface 0.0.0.0 --port 8080'
+alias zola-run-wsl='docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app -p 8080:8080 ghcr.io/getzola/zola:v0.19.1 serve --interface 0.0.0.0 --port 8080 --base-url "http://127.0.0.1"'
